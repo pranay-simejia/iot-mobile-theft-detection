ab72ed294f36bf778933f89ff12893c581fb92cc
Pratik <pmprateekmohan@outlook.com>
2022-12-09 17:07:02 +0530

    priv levels added

--- contracts/LostMobileList.sol
+++ contracts/LostMobileList.sol
@@ -1,4 +1,6 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2; //typeerror ke liye tha
+
 contract LostMobileList{
     uint public lostMobileCount = 0;
     
@@ -29,14 +31,38 @@ contract LostMobileList{
     string mobileNumber,
     string location  
   );
-  
+  //will optimise subha
+  struct Operator{
+    string operator;
+    uint weight; //1: edit, add, delete, 2: GSM
+  }
+  mapping(address=> Operator) public users; //map for address and users
+  address public gsm;
+
+  constructor() public {
+    gsm=msg.sender;
+    users[msg.sender].weight=2;
+
+  }
+  function addOperator(address addr, Operator memory company) public {
+     require(msg.sender==gsm,'Must be GSM');
+      users[addr]=company;
+  }
+  function removeOperator(address addr, Operator memory company) public {
+    require(msg.sender==gsm,'Must be GSM');
+    delete(users[addr]);
+    
+  }  
+
   function addLostMobile(string memory _IMEI,string memory _mobileNumber,string memory _location) public {
+    require(user[msg.sender].weight>=1,'Only Operators can add Lost Mobiles');
     lostMobileCount ++;
     LostMobiles[lostMobileCount] = LostMobile(lostMobileCount,_IMEI,_mobileNumber,_location);
     emit LostMobileAdded(lostMobileCount, _IMEI, _mobileNumber, _location);
   }
 
   function removeLostMobile(string memory _IMEI) public {
+    require(user[msg.sender].weight>=1,'Only Operators and GSM can remove Lost Mobiles');
     for(uint i = 1; i<= lostMobileCount; i++){
         if(keccak256(abi.encodePacked(LostMobiles[i].IMEI)) == (keccak256(abi.encodePacked(_IMEI)))) {
             delete LostMobiles[i];
@@ -48,6 +74,7 @@ contract LostMobileList{
   }
 
 function editLostMobile(string memory _IMEI,string memory _mobileNumber,string memory _location) public {
+   require(user[msg.sender].weight>=1,'Only Operators and GSM can remove Lost Mobiles');
     for(uint i = 1; i<= lostMobileCount; i++){
         if(keccak256(abi.encodePacked(LostMobiles[i].IMEI)) == (keccak256(abi.encodePacked(_IMEI)))){
             
