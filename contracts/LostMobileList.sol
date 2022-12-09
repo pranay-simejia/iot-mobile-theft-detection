fb023f7b9cebf9c106c6d89bc16e89099d9e7a28
Pranay Simejia <f20190267@pilani.bits-pilani.ac.in>
2022-12-09 16:57:47 +0530

    integeration

--- contracts/LostMobileList.sol
+++ contracts/LostMobileList.sol
@@ -47,14 +47,26 @@ contract LostMobileList{
     emit LostMobileRemoved(_IMEI);
   }
 
-  function editLostMobile(string memory _IMEI,string memory _mobileNumber,string memory _location) public {
+function editLostMobile(string memory _IMEI,string memory _mobileNumber,string memory _location) public {
     for(uint i = 1; i<= lostMobileCount; i++){
         if(keccak256(abi.encodePacked(LostMobiles[i].IMEI)) == (keccak256(abi.encodePacked(_IMEI)))){
-            LostMobiles[i] = LostMobile(i, _IMEI, _mobileNumber, _location);
-            emit LostMobileEditted(i,_IMEI, _mobileNumber, _location);
+            
+            string memory mobNum = _mobileNumber;
+            string memory loc = _location;
+            string memory empty = "";
+
+            if(keccak256(abi.encodePacked(_mobileNumber)) == (keccak256(abi.encodePacked(empty)))){
+              mobNum = LostMobiles[i].mobileNumber;
+            }
+
+            if(keccak256(abi.encodePacked(_location)) == (keccak256(abi.encodePacked(empty)))){
+              loc = LostMobiles[i].location;
+            }
+            
+            LostMobiles[i] = LostMobile(i, _IMEI, mobNum, loc);
+            emit LostMobileEditted(i, _IMEI, mobNum, loc);
             break;
         }
-  }
-}
+  }}
 
 }
--- src/app.js
+++ src/app.js
@@ -49,12 +49,13 @@ App = {
   
     loadContract: async () => {
       // Create a JavaScript version of the smart contract
-      const todoList = await $.getJSON('TodoList.json')
-      App.contracts.TodoList = TruffleContract(todoList)
-      App.contracts.TodoList.setProvider(App.web3Provider)
-  
+      const LostMobileList = await $.getJSON('LostMobileList.json')
+      App.contracts.LostMobileList = TruffleContract(LostMobileList)
+      App.contracts.LostMobileList.setProvider(App.web3Provider)
+      
       // Hydrate the smart contract with values from the blockchain
-      App.todoList = await App.contracts.TodoList.deployed()
+      App.LostMobileList = await App.contracts.LostMobileList.deployed()
+      console.log(`App.LostMobileList`)
     },
   
     render: async () => {
@@ -67,61 +68,93 @@ App = {
       App.setLoading(true)
   
       // Render Account
-      $('#account').html(App.account)
+      $('#accountAddress').html(App.account)
   
       // Render Tasks
-      await App.renderTasks()
-  
+      // await App.renderTasks()
+      
       // Update loading state
       App.setLoading(false)
     },
   
-    renderTasks: async () => {
-      // Load the total task count from the blockchain
-      const taskCount = await App.todoList.taskCount()
-      const $taskTemplate = $('.taskTemplate')
+    // renderTasks: async () => {
+    //   // Load the total task count from the blockchain
+    //   const taskCount = await App.todoList.taskCount()
+    //   const $taskTemplate = $('.taskTemplate')
   
-      // Render out each task with a new task template
-      for (var i = 1; i <= taskCount; i++) {
-        // Fetch the task data from the blockchain
-        const task = await App.todoList.tasks(i)
-        const taskId = task[0].toNumber()
-        const taskContent = task[1]
-        const taskCompleted = task[2]
+    //   // Render out each task with a new task template
+    //   for (var i = 1; i <= taskCount; i++) {
+    //     // Fetch the task data from the blockchain
+    //     const task = await App.todoList.tasks(i)
+    //     const taskId = task[0].toNumber()
+    //     const taskContent = task[1]
+    //     const taskCompleted = task[2]
   
-        // Create the html for the task
-        const $newTaskTemplate = $taskTemplate.clone()
-        $newTaskTemplate.find('.content').html(taskContent)
-        $newTaskTemplate.find('input')
-                        .prop('name', taskId)
-                        .prop('checked', taskCompleted)
-                        .on('click', App.toggleCompleted)
+    //     // Create the html for the task
+    //     const $newTaskTemplate = $taskTemplate.clone()
+    //     $newTaskTemplate.find('.content').html(taskContent)
+    //     $newTaskTemplate.find('input')
+    //                     .prop('name', taskId)
+    //                     .prop('checked', taskCompleted)
+    //                     .on('click', App.toggleCompleted)
   
-        // Put the task in the correct list
-        if (taskCompleted) {
-          $('#completedTaskList').append($newTaskTemplate)
-        } else {
-          $('#taskList').append($newTaskTemplate)
-        }
+    //     // Put the task in the correct list
+    //     if (taskCompleted) {
+    //       $('#completedTaskList').append($newTaskTemplate)
+    //     } else {
+    //       $('#taskList').append($newTaskTemplate)
+    //     }
   
-        // Show the task
-        $newTaskTemplate.show()
+    //     // Show the task
+    //     $newTaskTemplate.show()
+    //   }
+    // },
+  
+    addLostMobile: async () => {
+       App.button = 'add'; 
+    },
+    editLostMobile: async () => {
+       App.button = 'edit'; 
+    },
+    deleteLostMobile: async () => {
+       App.button = 'delete'; 
+       $('#mobileNumber').disabled = true;
+       $('#location').disabled = true;
+    },
+    searchLostMobile: async () => {
+       App.button = 'delete'; 
+       $('#mobileNumber').disabled = true;
+       $('#location').disabled = true;
+    },
+    submitClicked: async ()=>{
+      if(App.button=='add'){
+          const IMEI = $('#IMEI').val()
+          const mobileNumber = $('#mobileNumber').val()
+          const location = $('#location').val()
+          await App.LostMobileList.addLostMobile(IMEI,mobileNumber,location)
+          $('status-id').val() = 'Accepted'
+          window.location.reload()
+      }
+        else if(App.button=='edit'){
+          const IMEI = $('#IMEI').val()
+          const mobileNumber = $('#mobileNumber').val()
+          const location = $('#location').val()
+          await App.LostMobileList.editLostMobile(IMEI,mobileNumber,location)
+          $('status-id').val() = 'Accepted'
+          window.location.reload()
+      }else if(App.button=='delete'){
+        const IMEI = $('#IMEI').val()
+        await App.LostMobileList.deleteLostMobile(IMEI,mobileNumber,location)
+        window.location.reload()
+      }
+      else if(App.button=='search'){
+        const IMEI = $('#IMEI').val()
+        const {lostMobileCount} = App.LostMobileList
+      const {LostMobiles} =  App.LostMobileList
+        console.log(LostMobiles)
       }
     },
-  
-    createTask: async () => {
-      App.setLoading(true)
-      const content = $('#newTask').val()
-      await App.todoList.createTask(content)
-      window.location.reload()
-    },
-  
-    toggleCompleted: async (e) => {
-      App.setLoading(true)
-      const taskId = e.target.name
-      await App.todoList.toggleCompleted(taskId)
-      window.location.reload()
-    },
+
   
     setLoading: (boolean) => {
       App.loading = boolean
